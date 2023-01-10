dd4d653a6b99384a62f4822aebf94c7ecf5c0f4a
Marcus <txk.marcus@gmail.com>
2023-01-10 22:27:53 +0900

    Add live trackers for tokens

--- src/components/tickerRow.js
+++ src/components/tickerRow.js
@@ -1,32 +1,27 @@
-import {
-    useColorModeValue,
-    Tr,
-    Td
-  } from '@chakra-ui/react';
+import { useColorModeValue, Tr, Td } from "@chakra-ui/react";
 
-export default function TickerRow({ticker, walletContents, rowClick})
-{
-    return (
-        <Tr 
-            bg={useColorModeValue('gray.100', 'gray.700')}
-            onClick={() => rowClick(ticker.symbol)}
-            _hover={{
-                bg: useColorModeValue('gray.600', 'gray.200'),
-                color: useColorModeValue('gray.200', 'gray.800')
-            }}
-            >
-            <Td>
-                {ticker.symbol}
-            </Td>
-            <Td>
-                {ticker.poolPrice}
-            </Td>
-            <Td>   
-                {ticker.oraclePrice}
-            </Td>
-            <Td isNumeric>
-                {walletContents.has(ticker.symbol) ? walletContents.get(ticker.symbol) : 0}
-            </Td>
-        </Tr>
-    );
+export default function TickerRow({
+  symbol,
+  oraclePrice,
+  oracleLastUpdated,
+  walletContents,
+  rowClick,
+}) {
+  return (
+    <Tr
+      bg={useColorModeValue("gray.100", "gray.700")}
+      onClick={() => rowClick(symbol)}
+      _hover={{
+        bg: useColorModeValue("gray.600", "gray.200"),
+        color: useColorModeValue("gray.200", "gray.800"),
+      }}
+    >
+      <Td>{symbol}</Td>
+      <Td>{oraclePrice}</Td>
+      <Td>{oracleLastUpdated}</Td>
+      <Td isNumeric>
+        {walletContents.has(symbol) ? walletContents.get(symbol) : 0}
+      </Td>
+    </Tr>
+  );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+// Should be shifted to ENV for production
+export const ASSETS = [
+  { symbol: "AAPL", contract: "0x139C8512Cde1778e9b9a8e721ce1aEbd4dD43587" },
+  { symbol: "TSLA", contract: "0x1ceDaaB50936881B3e449e47e40A2cDAF5576A4a" },
+  { symbol: "MSFT", contract: "0x021Fb44bfeafA0999C7b07C4791cf4B859C3b431" },
+];
