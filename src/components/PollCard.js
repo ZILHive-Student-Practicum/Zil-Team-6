b1ce06bc5d801fd3e21128de14f47b07ba579c7e
joash-lee <91481123+joash-lee@users.noreply.github.com>
2023-01-12 13:46:16 +0700

    Fixed Bugs

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -1,67 +1,78 @@
 import React from "react";
-import { Box, Heading, Text, Flex, Stack, Card, Badge, Button } from "@chakra-ui/react";
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Box, Heading, Text, Flex, Stack, Card, Badge } from "@chakra-ui/react";
+import StyleSheet from "react";
 
 const cardStyle = {
-    shadowColor: '#171717',
-    shadowOffset: {width: -2, height: 4},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    boxShadow: '1px 2px 9px #88f797',
-}
+  shadowColor: "#171717",
+  shadowOffset: { width: -2, height: 4 },
+  shadowOpacity: 0.2,
+  shadowRadius: 3,
+  boxShadow: "1px 2px 9px #88f797",
+};
 
 export default function PollCard({
   id = 0,
   status = "",
   title = "hello",
   description = "desc",
-  VoteinFavour = 60,
 }) {
-    return (
-      <Box>
+  // if (status === "In progress"){
+  return (
+    <Box>
       <Card padding="20px" style={cardStyle}>
         <Stack spacing="10">
           <Flex justifyContent="space-between">
-            <Heading
-              as="h2"
-              fontWeight="bold"
-              fontSize="12px"
-              isTruncated
-            >
+            <Heading as="h2" fontWeight="bold" fontSize="12px" isTruncated>
               ID: {id}
             </Heading>
-            <Badge colorScheme='green'>
-              <Text as="span">
-                {status}
-              </Text>
+            <Badge colorScheme="green">
+              <Text as="span">{status}</Text>
             </Badge>
           </Flex>
 
-          <Text as="span" fontSize='xl'>
-            {title}
-          </Text>
-          
-          <Stack spacing={1}>
-              <Text fontSize='xs' color= 'grey'>Description:</Text>
-              <Text fontSize='xs' color= 'grey'>{description}</Text>         
-          </Stack>
-          <ProgressBar>
-            <ProgressBar striped variant="success" now={VoteinFavour} label={`${VoteinFavour}% YES`}/>
-          </ProgressBar>
+          <Flex fontSize="16px">{title}</Flex>
 
-          <Stack spacing={1} direction='row' align='center'>
-            <Text>Vote: </Text>
-            <Button colorScheme='teal' size='sm'>
-              Yes
-            </Button>
-            
-            <Button colorScheme='teal' size='sm'>
-              No
-            </Button>
-          </Stack>
+          <Flex justifyContent="space-between">
+            <Text as="span">{description}</Text>
+          </Flex>
         </Stack>
       </Card>
-      </Box>
-    );
+    </Box>
+  );
+  // }
+
+  // return (
+  //   <Box>
+  //   <Card padding="20px">
+  //     <Stack spacing="10">
+  //       <Flex justifyContent="space-between">
+  //         <Heading
+  //           as="h2"
+  //           fontWeight="bold"
+  //           fontSize="12px"
+  //           isTruncated
+  //         >
+  //           ID: {id}
+  //         </Heading>
+
+  //         <Badge colorScheme='red'>
+  //           <Text as="span">
+  //             {status}
+  //           </Text>
+  //         </Badge>
+  //       </Flex>
+
+  //       <Flex fontSize="16px">
+  //           {title}
+  //       </Flex>
+
+  //       <Flex justifyContent="space-between">
+  //         <Text as="span">
+  //           {description}
+  //         </Text>
+  //       </Flex>
+  //     </Stack>
+  //   </Card>
+  //   </Box>
+  // );
 }
